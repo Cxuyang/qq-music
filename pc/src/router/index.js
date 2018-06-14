058420bcb5d91b4aef735c7766144b1d5fcb5a5b
Cxuyang <caoxuyang0317@qq.com>
2018-06-14 14:21:37 +0800

    实现vue-router懒加载

--- pc/src/router/index.js
+++ pc/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import PCMusicstore from './../components/pc_components/pc_musicstore'
-import PCMymusic from './../components/pc_components/pc_mymusic'
-//
-import PCMSdefault from './../components/pc_components/pc_mstore_component/pc_ms_default/pc_ms_default.vue'
-import PCMSsinger from './../components/pc_components/pc_mstore_component/pc_ms_singer/pc_ms_singer.vue'
-import PCMSalbum from './../components/pc_components/pc_mstore_component/pc_ms_album/pc_ms_album.vue'
-import PCMSSongList from './../components/pc_components/pc_mstore_component/pc_ms_songList/pc_ms_songList.vue'
-import PCMSradio from './../components/pc_components/pc_mstore_component/pc_ms_radio/pc_ms_radio.vue'
-import PCMSmv from './../components/pc_components/pc_mstore_component/pc_ms_mv/pc_ms_mv.vue'
-import PCSingerDetail from './../components/pc_components/pc_singerDetail/pc_singerDetail.vue'
+// import PCMusicstore from './../components/pc_components/pc_musicstore'
+// import PCMymusic from './../components/pc_components/pc_mymusic'
+// //
+// import PCMSdefault from './../components/pc_components/pc_mstore_component/pc_ms_default/pc_ms_default.vue'
+// import PCMSsinger from './../components/pc_components/pc_mstore_component/pc_ms_singer/pc_ms_singer.vue'
+// import PCMSalbum from './../components/pc_components/pc_mstore_component/pc_ms_album/pc_ms_album.vue'
+// import PCMSSongList from './../components/pc_components/pc_mstore_component/pc_ms_songList/pc_ms_songList.vue'
+// import PCMSradio from './../components/pc_components/pc_mstore_component/pc_ms_radio/pc_ms_radio.vue'
+// import PCMSmv from './../components/pc_components/pc_mstore_component/pc_ms_mv/pc_ms_mv.vue'
+// import PCSingerDetail from './../components/pc_components/pc_singerDetail/pc_singerDetail.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -24,41 +24,41 @@ export default new Router({
     ,{
       path: '/PCMusicstore',
       name: 'PCMusicstore',
-      component: PCMusicstore,
+      component: (resolve) => require(['./../components/pc_components/pc_musicstore'], resolve),
       children:[
         {
           path: '/PCMusicstore/PCMSdefault',
           name: 'PCMSdefault',
-          component: PCMSdefault
+          component: (resolve) => require(['./../components/pc_components/pc_mstore_component/pc_ms_default/pc_ms_default.vue'], resolve),
         },{
           path: '/PCMusicstore/PCMSsinger',
           name: 'PCMSsinger',
-          component: PCMSsinger
+          component: (resolve) => require(['./../components/pc_components/pc_mstore_component/pc_ms_singer/pc_ms_singer.vue'], resolve),
         },{
           path: '/PCMusicstore/PCMSalbum',
           name: 'PCMSalbum',
-          component: PCMSalbum
+          component: (resolve) => require(['./../components/pc_components/pc_mstore_component/pc_ms_album/pc_ms_album.vue'], resolve),
         },{
           path: '/PCMusicstore/PCMSSongList',
           name: 'PCMSSongList',
-          component: PCMSSongList
+          component: (resolve) => require(['./../components/pc_components/pc_mstore_component/pc_ms_songList/pc_ms_songList.vue'], resolve),
         },{
           path: '/PCMusicstore/PCMSradio',
           name: 'PCMSradio',
-          component: PCMSradio
+          component: (resolve) => require(['./../components/pc_components/pc_mstore_component/pc_ms_radio/pc_ms_radio.vue'], resolve),
         },{
           path: '/PCMusicstore/PCMSmv',
           name: 'PCMSmv',
-          component: PCMSmv
+          component: (resolve) => require(['./../components/pc_components/pc_mstore_component/pc_ms_mv/pc_ms_mv.vue'], resolve),
         },{
           path: '/PCMusicstore/PCSingerDetail/:singermid',
           name: 'PCSingerDetail',
-          component: PCSingerDetail,
+          component: (resolve) => require(['./../components/pc_components/pc_singerDetail/pc_singerDetail.vue'], resolve),
         }]
     },{
       path: '/PCMymusic',
       name: 'PCMymusic',
-      component: PCMymusic
+      component: (resolve) => require(['./../components/pc_components/pc_mymusic'], resolve),
     }    
   ]
 })
